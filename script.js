4e070d572019756237b17296482d5d6a9144cb59
CattacoCattaco <cattacoiscool@gmail.com>
2025-05-17 21:52:38 -0700

    Add save/load to/from file funcs

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
-function sayHi() {
-  console.log("hi");
+const downloadToFile = (content, filename, contentType) => {
+  const a = document.createElement('a');
+  const file = new Blob([content], { type: contentType });
+
+  a.href = URL.createObjectURL(file);
+  a.download = filename;
+  a.click();
+
+  URL.revokeObjectURL(a.href);
+};
+
+var selectedFile;
+
+// get the value every time the user selects a new file
+file_input.addEventListener("change", (e) => {
+  // e.target points to the input element
+  selectedFile = e.target.files[0]
+})
+
+function saveAllDataToFile()
+{
+  //Data to be stored in save file
+  var data = "";
+
+  for(var i = 0; i < localStorage.length; i++)
+  {
+    var key = localStorage.key(i)
+
+    data += `${key}=${localStorage.getItem(key)}\n`
+  }
+
+  data.trimEnd();
+
+  // Write data in 'CattacoCattaco_github_io_save.txt'
+  downloadToFile(data, 'CattacoCattaco_github_io_save.txt', 'text/plain')
+}
+
+function loadAllDataFromFile()
+{
+  if (!selectedFile) return;
+
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    // e.target points to the reader
+    const textContent = e.target.result
+    var data = textContent.split("\n");
+
+    for(var entry in data)
+    {
+      entry = data[entry].split("=");
+
+      localStorage.setItem(entry[0], entry[1]);
+    }
+  }
+
+  reader.onerror = (e) => {
+    const error = e.target.error
+    console.error(`Error occured while reading ${file.name}`, error)
+  }
+
+  reader.readAsText(selectedFile);
 }
